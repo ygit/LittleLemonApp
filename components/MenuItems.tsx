6f6498fdd4c6f015c0ca3cca8c2c3cb14b8e996c
Yogesh Singh <yogesh.singh.2810@gmail.com>
2024-03-17 14:45:23 +0530

    resolved type declaration

--- components/MenuItems.tsx
+++ components/MenuItems.tsx
@@ -1,38 +1,47 @@
-import { View, Text, StyleSheet, ScrollView, FlatList } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 
 const menuItemsToDisplay = [
-  { name: "Hummus", id: "1A" },
-  { name: "Moutabal", id: "2B" },
-  { name: "Falafel", id: "3C" },
-  { name: "Marinated Olives", id: "4D" },
-  { name: "Kofta", id: "5E" },
-  { name: "Eggplant Salad", id: "6F" },
-  { name: "Lentil Burger", id: "7G" },
-  { name: "Smoked Salmon", id: "8H" },
-  { name: "Kofta Burger", id: "9I" },
-  { name: "Turkish Kebab", id: "10J" },
-  { name: "Fries", id: "11K" },
-  { name: "Buttered Rice", id: "12L" },
-  { name: "Bread Sticks", id: "13M" },
-  { name: "Pita Pocket", id: "14N" },
-  { name: "Lentil Soup", id: "15O" },
-  { name: "Greek Salad", id: "16Q" },
-  { name: "Rice Pilaf", id: "17R" },
-  { name: "Baklava", id: "18S" },
-  { name: "Tartufo", id: "19T" },
-  { name: "Tartufo", id: "20U" },
-  { name: "Tiramisu", id: "21V" },
-  { name: "Panna Cotta", id: "22W" },
+  { name: "Hummus", price: "$5.00", id: "1A" },
+  { name: "Moutabal", price: "$5.00", id: "2B" },
+  { name: "Falafel", price: "$7.50", id: "3C" },
+  { name: "Marinated Olives", price: "$5.00", id: "4D" },
+  { name: "Kofta", price: "$5.00", id: "5E" },
+  { name: "Eggplant Salad", price: "$8.50", id: "6F" },
+  { name: "Lentil Burger", price: "$10.00", id: "7G" },
+  { name: "Smoked Salmon", price: "$14.00", id: "8H" },
+  { name: "Kofta Burger", price: "$11.00", id: "9I" },
+  { name: "Turkish Kebab", price: "$15.50", id: "10J" },
+  { name: "Fries", price: "$3.00", id: "11K" },
+  { name: "Buttered Rice", price: "$3.00", id: "12L" },
+  { name: "Bread Sticks", price: "$3.00", id: "13M" },
+  { name: "Pita Pocket", price: "$3.00", id: "14N" },
+  { name: "Lentil Soup", price: "$3.75", id: "15O" },
+  { name: "Greek Salad", price: "$6.00", id: "16Q" },
+  { name: "Rice Pilaf", price: "$4.00", id: "17R" },
+  { name: "Baklava", price: "$3.00", id: "18S" },
+  { name: "Tartufo", price: "$3.00", id: "19T" },
+  { name: "Tiramisu", price: "$5.00", id: "20U" },
+  { name: "Panna Cotta", price: "$5.00", id: "21V" },
 ];
 export const MenuItems = () => {
-  const renderItem = ({ item }) => {
-    return <Text style={styles.item}>{item.name}</Text>;
+  const renderItem = ({ item }: { item: { name: String; price: String } }) => {
+    return (
+      <View style={styles.itemContainer}>
+        <Text style={styles.item}>{item.name}</Text>
+        <Text style={styles.item}>{item.price}</Text>
+      </View>
+    );
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Menu Items</Text>
-      <FlatList data={menuItemsToDisplay} renderItem={renderItem} />
+      <FlatList
+        style={styles.flatList}
+        data={menuItemsToDisplay}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
     </View>
   );
 };
@@ -49,7 +58,7 @@ const styles = StyleSheet.create({
     textAlign: "center",
     marginTop: 16,
   },
-  scrollView: {
+  flatList: {
     width: "100%",
   },
   item: {
@@ -57,5 +66,11 @@ const styles = StyleSheet.create({
     fontSize: 24,
     textAlign: "center",
     marginTop: 16,
+  },
+  itemContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 16,
   },
 });
